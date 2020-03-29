c1d1a9ff6843626e61e3d2bbc0cc28734f14a93f
angeloevangelista <angeloevan.ane@gmail.com>
2020-03-28 22:47:17 -0300

    removido scroll, mobile

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -64,7 +64,7 @@ export default function Incidents() {
         data={incidents}
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
-        // showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
         fadingEdgeLength={true}
         onEndReached={loadIncidents}
         onEndReachedThreshold={0.3}
